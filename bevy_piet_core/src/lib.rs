ef64de4a67011b855f1be0ab69e062fc862d6f54
Konstantin Yeftifeyev <zoxo@outlook.com>
2022-11-19 11:53:46 +0200

    feat: Commit svg code

--- bevy_piet_core/src/lib.rs
+++ bevy_piet_core/src/lib.rs
@@ -2,14 +2,14 @@ use bevy::{app::PluginGroupBuilder, prelude::*};
 
 use bevy_piet_render::PietRenderPlugin;
 use bevy_piet_text::PietTextPlugin;
-use bevy_piet_vector::PietVectorPlugin;
+// use bevy_piet_vector::PietVectorPlugin;
 
 pub struct BevyPietPlugins;
 
 impl PluginGroup for BevyPietPlugins {
     fn build(&mut self, group: &mut PluginGroupBuilder) {
         group.add(PietRenderPlugin::default());
-        group.add(PietVectorPlugin::default());
+        // group.add(PietVectorPlugin::default());
         group.add(PietTextPlugin::default());
     }
 }
